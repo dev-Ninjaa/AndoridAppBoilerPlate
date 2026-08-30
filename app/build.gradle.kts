6d5f4fe9f3ac11124b70f5b69408a8b0f28437f2
alakaloops <dashalaka59@gamil.com>
2026-08-30 19:50:05 +0100

    Fix resource linking theme error and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
